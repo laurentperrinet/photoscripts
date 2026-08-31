669e796aa2ab79a84f15e62cc066786a638a8161
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2026-08-31 18:49:00 +0200

    Update README and refactor rename_exif.py with argparse and verbose mode
    Expand README with a “Files” section listing each script and its purpose.  [K
    
    Add detailed module docstrings, supported extension lists, and usage instru[6D[K
    instructions to rename_exif.py.
    Refactor sortPhotos to take a verbose flag, improve date‑extraction logic, [K
    and clean up filename handling.
    Replace manual sys.argv parsing with argparse, introducing –dry-run and –ve[3D[K
    –verbose options.

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # photoscripts
 
-a bunch of simplistic python scripts to manage a photo library:
+a collection of Python scripts to manage a photo/video library:
 
-- rename pictures and movies in a folder by prepending the date in ISO format (that is, 2013-12-25 for christmas' 13)
-- rotate movies
-- transfer files from a SONY CyberShot camera with unorthodox naming scheme
+## Files
+
+- **`rename_file.py`** - Renames files by replacing "Photo" and "Vidéo" prefixes in filenames
+- **`rename_exif.py`** - Organizes media files by prepending their creation date (from EXIF metadata or file modification time) in ISO 8601 format
+- **`rotate_video.py`** - Rotates videos using ffmpeg (0=90° counter-clockwise+flip, 1=90° clockwise, 2=90° counter-clockwise, 3=90° clockwise+flip)
 
 ## Installation
 
--- rename_exif.py
+++ rename_exif.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-   rename_exif.py
+rename_exif.py
 
-A (simple) library to organize media files according to their creation date.
+A library to organize media files according to their creation date.
 
 The core idea is to rename pictures and movies in a folder by prepending the
 date in ISO 8601 format (that is, 2013-12-25 for the 25th of december of 2013).
@@ -12,34 +12,74 @@ https://fr.wikipedia.org/wiki/ISO_8601
 
 e.g. 1977-04-22T06:00:00Z
 
+Supported picture extensions: jpg, jpeg, png
+Supported movie extensions: mp4, mpg, mov, 3gp
+Meta/AAC extensions: AAE
+
+The date extraction priority order is:
+1. EXIF DateTimeOriginal (primary)
+2. EXIF DateTime (fallback)
+3. EXIF DateTimeModified (fallback)
+4. File modification time (last resort)
+5. Movie creation_time via ffprobe (for mp4/mpg/mov/3gp files)
+
+Usage:
+    python3 rename_exif.py [-d] [-v] [path [path ...]]
+
+    -d, --dry-run  : don't actually rename files, just show what would be done
+    -v, --verbose    : print debug information
+    path             : one or more file patterns or folder paths to process
+
+If no path is given, the script prints the usage message.
 """
-DEBUG = True
+
 DEBUG = False
-# exif_sort[January 2012] / martin gehrke [martin AT teamgehrke.com]
-# sorts jpg/jpegs into date folders based on exif data
-EXTENSIONS_pict =['jpg','jpeg', 'png']
-EXTENSIONS_movie =['mp4', 'mpg', 'mov', '3gp']
+
+# Supported file extensions (lowercase and uppercase)
+EXTENSIONS_pict = ['jpg', 'jpeg', 'png']
+EXTENSIONS_movie = ['mp4', 'mpg', 'mov', '3gp']
 EXTENSIONS = []
 for EXTENSIONS_ in [EXTENSIONS_pict, EXTENSIONS_movie]:
     [EXTENSIONS.append(ext) for ext in EXTENSIONS_]
     [EXTENSIONS.append(ext.upper()) for ext in EXTENSIONS_]
-if DEBUG: print('DEBUG extensions = ', EXTENSIONS)
 
-# some files come with some meta data
-EXTENSIONS_meta =['AAE']
-if DEBUG: print('DEBUG meta extensions = ', EXTENSIONS_meta)
+# Meta file extensions (sidecar files)
+EXTENSIONS_meta = ['AAE']
+
 
 from PIL import Image
 from PIL.ExifTags import TAGS
 import sys, os, glob
 
 import datetime
+
+
 def modification_date(filename):
+    """Return the file's modification date as an ISO 8601 string.
+
+    Args:
+        filename (str): Path to the file.
+
+    Returns:
+        str: Modification date as returned by datetime.fromtimestamp.
+    """
     t = os.path.getmtime(filename)
     return str(datetime.datetime.fromtimestamp(t))
 
+
 def get_exif(fn):
-#see <a href="http://www.blog.pythonlibrary.org/2010/03/28/getting-photo-metadata-exif-using-python/">http://www.blog.pythonlibrary.org/2010/03/28/getting-photo-metadata-exif-using-python/</a>
+    """Extract EXIF metadata from an image file.
+
+    Uses PIL to read EXIF tags from JPEG/JPEG images and decodes tag names
+    using PIL.ExifTags.TAGS.
+
+    Args:
+        fn (str): Path to the image file.
+
+    Returns:
+        dict: Mapping of decoded tag names to their values. Returns empty dict
+        if the file has no EXIF data or cannot be opened.
+    """
     ret = {}
     i = Image.open(fn)
     try:
@@ -48,41 +88,82 @@ def get_exif(fn):
             decoded = TAGS.get(tag, tag)
             ret[decoded] = value
         return ret
-    except Exception as e:
-        if DEBUG: print('DEBUG Picture ', fn, ' has no tag, error is: ', e)
+    except Exception:
         return {}
-    #print ret
+
 
 def get_exif_modification_date(filename, tag='EXIF DateTimeOriginal'):
+    """Read a specific EXIF tag from a file using SimpleCV.
+
+    Args:
+        filename (str): Path to the file.
+        tag (str): EXIF tag name to read. Defaults to 'EXIF DateTimeOriginal'.
+
+    Returns:
+        str: The unformatted printable value of the EXIF tag, or empty string
+        if the tag is not found.
+    """
     from SimpleCV import EXIF
     with open(filename, 'rb') as f:
         UNFORMATTED = EXIF.process_file(f, stop_tag=tag)[tag].printable
     return UNFORMATTED
 
+
 def format_dateTime(UNFORMATTED):
-    if DEBUG: print(UNFORMATTED)
+    """Format an unformatted date-time string into ISO 8601 format.
+
+    Handles both space-separated ('YYYY MM DD HH MM SS') and T-separated
+    ('YYYY-MM-DDTHH:MM:SS') formats.
+
+    Args:
+        UNFORMATTED (str): Raw date-time string from EXIF or ffprobe.
+
+    Returns:
+        str: Date in ISO 8601 format (YYYY-MM-DDTHH:MM:SS).
+    """
     try:
         DATE, TIME = UNFORMATTED.split()
     except ValueError:
         DATE, TIME = UNFORMATTED.split('T')
-    return DATE.replace(':','-') + 'T' + TIME[:8].replace(':', '')
+    return DATE.replace(':', '-') + 'T' + TIME[:8].replace(':', '')
+
 
 def get_movie_creation_date(fn):
+    """Extract the creation date from a movie file using ffprobe.
+
+    Looks for TAG:creation_time in the ffprobe output and formats it.
+
+    Args:
+        fn (str): Path to the movie file.
+
+    Returns:
+        str: Date in ISO 8601 format, or empty string if not found.
+    """
     for line in os.popen('ffprobe -loglevel quiet -show_format -i ' + fn).readlines():
-        #print line
         if line[:18] == 'TAG:creation_time=':
-            datetime = line[18:]
-            return format_dateTime(datetime)
+            datetime_str = line[18:]
+            return format_dateTime(datetime_str)
     return ''
 
-def sortPhotos(paths, dryrun):
-#     for root, dirs, files in os.walk(path):
-#         if not(root[-9:]=='.@__thumb'):
-#             PHOTOS = []
-#             for EXTENSION in EXTENSIONS:
-#                 PHOTO = glob.glob(os.path.join(root, '*%s' % EXTENSION))
-#                 PHOTOS.extend(PHOTO)
 
+def sortPhotos(paths, dryrun, verbose=False):
+    """Process files matching the given paths and rename them with their creation date.
+
+    For each file, extracts the creation date using EXIF data (pictures) or
+    ffprobe (movies), then prepends the date to the filename in ISO 8601 format.
+
+    Handles fallback chains when primary date sources are unavailable, and
+    cleans existing date strings from filenames to avoid duplicates.
+
+    Args:
+        paths (str or list): Single file pattern or list of file paths/folders.
+        dryrun (bool): If True, only show what would be renamed without actually
+            renaming files.
+        verbose (bool): If True, print debug information. Defaults to False.
+    """
+    if verbose:
+        global DEBUG
+        DEBUG = True
     for PHOTO in glob.glob(paths):
         # 1/ grab the creation date by heuristics
         # first process movies
@@ -90,106 +171,123 @@ def sortPhotos(paths, dryrun):
             DATETIME = get_movie_creation_date(PHOTO)
             if DATETIME == '':
                 DATETIME = format_dateTime(modification_date(PHOTO))
-        elif PHOTO.split('.')[-1].lower() in EXTENSIONS_pict: #else: #elif not( get_exif(PHOTO) == {}):
-            try: # trying first with SimpleCV
+        elif PHOTO.split('.')[-1].lower() in EXTENSIONS_pict:
+            try:  # trying first with SimpleCV
                 DATETIME = format_dateTime(get_exif_modification_date(PHOTO))
-            except:
-#                         print('DEBUG SimpleCV failed ')
-                try: # trying with PIL
-                    DATETIME = format_dateTime(get_exif(PHOTO)['DateTimeOriginal'])
-                except:
-                    try: # trying out another tag
-                        DATETIME = format_dateTime(get_exif(PHOTO)['DateTime'])
-                    except: # yet another one
-                        try:
-                            DATETIME = format_dateTime(get_exif(PHOTO)['DateTimeModified'])
-                        except: # file's modification time
+            except Exception:
+                try:  # trying with PIL
+                    exif = get_exif(PHOTO)
+                    DATETIME = format_dateTime(exif['DateTimeOriginal'])
+                except Exception:
+                    try:  # trying out another tag
+                        DATETIME = format_dateTime(exif['DateTime'])
+                    except Exception:
+                        try:  # yet another one
+                            DATETIME = format_dateTime(exif['DateTimeModified'])
+                        except Exception:  # file's modification time
                             try:
                                 DATETIME = format_dateTime(modification_date(PHOTO))
-                            except: #
+                            except Exception:
                                 print('Giving up :-/ ')
                                 DATETIME = None
         else:
             print('File ', PHOTO, ' not in the EXTENSION list')
             DATETIME = None
-        if DEBUG: print(DATETIME)
+
+        if DEBUG:
+            print(DATETIME)
+
         # 2/ prepend the creation date to the file name
         ROOT, FILE = os.path.split(PHOTO)
-        if not(DATETIME == None):
+        if not (DATETIME is None):
             FILE_ = FILE
-            if DEBUG: print(FILE_, DATETIME.replace('T', '_').replace('-', ''))
+            if DEBUG:
+                print(FILE_, DATETIME.replace('T', '_').replace('-', ''))
+
+            # Remove any existing occurrence of the date from the filename
             FILE_ = FILE_.replace(DATETIME.replace('T', '-'), '')
             FILE_ = FILE_.replace(DATETIME.replace('T', '_').replace('-', ''), '')
             FILE_ = FILE_.replace(DATETIME.replace('T', '_').replace('-', ''), '')
-            for sep in ['-', '_', '', '']: # TODO :test the following 3 lines
-                FILE_ = FILE_.replace(sep + DATETIME, '') # remove existing occurences of DATETIME
-                FILE_ = FILE_.replace(sep + DATETIME[:-1], '') # remove existing occurences of DATETIME
-                FILE_ = FILE_.replace(sep + DATETIME[:10], '') # remove existing occurences of DATETIME
-                FILE_ = FILE_.replace(sep + DATETIME.replace('-', '')[:8], '') # remove existing occurences of DATETIME
+
+            for sep in ['-', '_', '', '']:
+                FILE_ = FILE_.replace(sep + DATETIME, '')  # remove existing occurrences of DATETIME
+                FILE_ = FILE_.replace(sep + DATETIME[:-1], '')  # remove existing occurrences of DATETIME
+                FILE_ = FILE_.replace(sep + DATETIME[:10], '')  # remove existing occurrences of DATETIME
+                FILE_ = FILE_.replace(sep + DATETIME.replace('-', '')[:8], '')  # remove existing occurrences of DATETIME
                 FILE_ = FILE_.replace(sep + DATETIME[:-1].replace('-', '_'), '')
                 FILE_ = FILE_.replace(sep + DATETIME.replace('-', ''), '')
                 FILE_ = FILE_.replace('--', '-')
-            if DEBUG: print(FILE_.split('.')[0], DATETIME.replace('T', '_').replace('-', ''))
+
+            if DEBUG:
+                print(FILE_.split('.')[0], DATETIME.replace('T', '_').replace('-', ''))
+
             if len(FILE_.split('.')[0]) > 0:
                 SEP = '_'
             else:
                 SEP = ''
+
             newname = os.path.join(ROOT, "%s%s%s" % (DATETIME, SEP, FILE_))
+
+            # Normalize double separators
             for sep in ['-', '_']:
                 newname = newname.replace(sep*2, sep)
             newname = newname.replace('-_', '_')
             newname = newname.replace('_-', '_')
 
             N = len(DATETIME)
-            if not(DATETIME[:-1] == FILE_[:(N-1)]):
+            if not (DATETIME[:-1] == FILE_[:(N-1)]):
                 # in this case, it is different so, we apply the change
-                print('renaming ',  PHOTO, ' to ', newname)
-                if not(dryrun): os.rename(PHOTO, newname)
+                print('renaming ', PHOTO, ' to ', newname)
+                if not dryrun:
+                    os.rename(PHOTO, newname)
 
                 ext = PHOTO.split('.')[-1]
                 for ext_meta in EXTENSIONS_meta:
                     if os.path.isfile(PHOTO.replace(ext, ext_meta)):
                         # print('meta  ',  ext, ext_meta)
-                        print('meta renaming ',  PHOTO.replace(ext, ext_meta), ' to ', newname.replace(ext, ext_meta))
-                        if not(dryrun): os.rename(PHOTO.replace(ext, ext_meta), newname.replace(ext, ext_meta))
+                        print('meta renaming ', PHOTO.replace(ext, ext_meta), ' to ', newname.replace(ext, ext_meta))
+                        if not dryrun:
+                            os.rename(PHOTO.replace(ext, ext_meta), newname.replace(ext, ext_meta))
 
-            elif False: # TODO (DATETIME.replace('-', '') == FILE[:N_]):
-                # HACK : we were before using a version which was missing the dashes
+            elif False:  # TODO (DATETIME.replace('-', '') == FILE[:N_]):
+                # HACK: we were before using a version which was missing the dashes
                 # now, we have a correct ISO8601
-                print('upgrading ',  PHOTO, ' to ', newname)
-                if not(dryrun): os.rename(PHOTO, newname)
+                print('upgrading ', PHOTO, ' to ', newname)
+                if not dryrun:
+                    os.rename(PHOTO, newname)
             else:
-                print('already renamed ',  PHOTO, ' with date ', DATETIME[:-1])
+                print('already renamed ', PHOTO, ' with date ', DATETIME[:-1])
 
-if __name__=="__main__":
 
-    # import argparse
-    # # construct the argument parse and parse the arguments
-    # ap = argparse.ArgumentParser()
-    # ap.add_argument("-v", "--debug", action="store_true", default=False, required=False,
-    #     help="debug this script")
-    # ap.add_argument("-p", "--print", action="store_true", default=False, required=False,
-    #     help="just prints the command")
-    #
-    # args = vars(ap.parse_args())
+if __name__ == "__main__":
+    import argparse
 
-    args = sys.argv[1:]
+    parser = argparse.ArgumentParser(
+        description='Organize media files according to their creation date, '
+                    'prepending the date in ISO 8601 format.'
+    )
+    parser.add_argument(
+        '-d', '--dry-run',
+        action='store_true',
+        default=False,
+        required=False,
+        help='dry-run mode: show what would be renamed without actually renaming'
+    )
+    parser.add_argument(
+        '-v', '--verbose',
+        action='store_true',
+        default=False,
+        required=False,
+        help='print debug information'
+    )
+    parser.add_argument(
+        'paths',
+        metavar='PATH',
+        nargs='+',
+        help='file patterns or folder paths to process'
+    )
 
-    if not len(args):
-        print("""
-        Usage:
-            python3 rename_exif.py [-d] 'pattern'
+    args = parser.parse_args()
 
-            -d: dry-run mode
-            """)
-    else:
-        dryrun = args[0]
-        PATHS = args[1:]
-        if dryrun != '-d':
-            dryrun = ''
-            PATHS = args
-        if DEBUG:
-            if (dryrun== '-d'): print('DEBUG: dryrun mode')
-        for PATH in PATHS:
-#             print 'Processing path ', PATH
-            sortPhotos(PATH, dryrun=(dryrun=='-d'))
+    for PATH in args.paths:
+        sortPhotos(PATH, dryrun=(args.dry_run), verbose=args.verbose)
